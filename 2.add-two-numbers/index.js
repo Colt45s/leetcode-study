719179ee9bc3170ad9edb04958ad6be9ea0b5da1
k-yawata <kzk0693@gmail.com>
2020-05-28 23:48:09 +0900

    update: 2

--- 2.add-two-numbers/index.js
+++ 2.add-two-numbers/index.js
@@ -15,35 +15,23 @@ var addTwoNumbers = function (l1, l2) {
   let dummy = result;
   let carry = 0;
 
-  while (l1 && l2) {
-    const sum = l1.val + l2.val + carry;
+  while (l1 || l2) {
+    const v1 = l1 ? l1.val : 0;
+    const v2 = l2 ? l2.val : 0;
+    const sum = v1 + v2 + carry;
     const digit = sum % 10;
-    carry = Math.floor(sum / 10);
+    carry = ~~(sum / 10);
     const now = new ListNode(digit);
     dummy.next = now;
     dummy = dummy.next;
-    l1 = l1.next;
-    l2 = l2.next;
-  }
 
-  while (l1) {
-    const sum = l1.val + carry;
-    const digit = sum % 10;
-    carry = Math.floor(sum / 10);
-    const now = new ListNode(digit);
-    dummy.next = now;
-    dummy = dummy.next;
-    l1 = l1.next;
-  }
+    if (l1) {
+      l1 = l1.next;
+    }
 
-  while (l2) {
-    const sum = l2.val + carry;
-    const digit = sum % 10;
-    carry = Math.floor(sum / 10);
-    const now = new ListNode(digit);
-    dummy.next = now;
-    dummy = dummy.next;
-    l2 = l2.next;
+    if (l2) {
+      l2 = l2.next;
+    }
   }
 
   if (carry) {
